53e24aaa001f703c61db6028fb498e3268abb722
barhmate <barhmate.mohamed@gmail.com>
2023-12-07 00:15:23 -0500

    creation PageCommandes

---
+++ config/commandes.php
@@ -0,0 +1,41 @@
+<?php 
+  function ajouterProduct($name, $quantity, $price, $img_url, $description)
+  {
+    if(require("connexion.php"))
+    {
+      $req = $access->prepare("INSERT INTO product (nom, quantity, price,, img_url, descriptionn) VALUES (?, ?, ?, ?)");
+
+      $req->execute(array($name, $quantity, $price, $img_url, $description));
+
+      $req->closeCursor();
+    }
+  }
+
+function afficherProduct()
+{
+	if(require("connexion.php"))
+	{
+		$req=$access->prepare("SELECT * FROM product ORDER BY id DESC");
+
+        $req->execute();
+
+        $data = $req->fetchAll(PDO::FETCH_OBJ);
+
+        return $data;
+
+        $req->closeCursor();
+	}
+}
+function supprimer($id)
+{
+	if(require("connexion.php"))
+	{
+		$req=$access->prepare("DELETE FROM product WHERE id=?");
+
+		$req->execute(array($id));
+
+		$req->closeCursor();
+	}
+}
+
+?>
