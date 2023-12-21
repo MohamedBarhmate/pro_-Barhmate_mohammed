c2fc2adb199b9ffa64a6a6ee76987a47a428c4e8
barhmate <barhmate.mohamed@gmail.com>
2023-12-20 23:28:02 -0500

    Update upgrade user

--- registration/admin/upgrade_user.php
+++ registration/admin/upgrade_user.php
@@ -2,48 +2,48 @@
 // Start a session
 session_start();
 
-// Check if the user is authenticated
+// Vérifiez si l'utilisateur est authentifié
 if (!isset($_SESSION['user_id'])) {
-    // Redirect to the login page if the user is not authenticated
+    // Redirection vers la page de connexion si l'utilisateur n'est pas authentifié
     header("Location: ../auth/login.php");
     exit();
 }
 
-// Get the user role from the session
+// Récupérer le rôle utilisateur de la session
 $user_role = $_SESSION['user_role'];
 
-// Check if the user has the administrator role
+// Vérifiez si l'utilisateur a le rôle d'administrateur
 if ($user_role != 1) {
-    // Redirect to the home page if the user is not an administrator
+    // Redirection vers la page d'accueil si l'utilisateur n'est pas administrateur
     header("Location: ../../index.php");
     exit();
 }
 
-// Check if the user ID to update is provided
+// Vérifiez si l'ID utilisateur à mettre à jour est fourni
 if (isset($_GET['user_id'])) {
     // Get the user ID from the URL
     $user_id = $_GET['user_id'];
 
-    // Connect to the database (adjust the path based on your file structure)
+    //Connectez-vous à la base de données (ajustez le chemin en fonction de la structure de vos fichiers)
     require_once('../../config/connexion.php');
 
-    // Query to update the user's role to 'admin'
+    // Requête pour mettre à jour le rôle de l'utilisateur en « admin »
     $update_query = "UPDATE `user` SET `role_id` = 1 WHERE `id` = $user_id";
     $result = mysqli_query($conn, $update_query);
 
-    // Check if the update was successful
+    // Vérifiez si la mise à jour a réussi
     if ($result) {
-        // Redirect to the dashboard after a successful update
+        // Redirection vers le tableau de bord après une mise à jour réussie
         header("Location: Tableau_bord.php");
     } else {
-        // Display an error message if there's an issue with the database update
+        // Afficher un message d'erreur en cas de problème avec la mise à jour de la base de données
         echo "Error updating user role: " . mysqli_error($conn);
     }
 
-    // Close the database connection
+    // Fermer la connexion à la base de données
     mysqli_close($conn);
 } else {
-    // Display a message if the user ID is not provided
+    // Afficher un message si l'ID utilisateur n'est pas fourni
     echo "User ID not provided.";
 }
 ?>
